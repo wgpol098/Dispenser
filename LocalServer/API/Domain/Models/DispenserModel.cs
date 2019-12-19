bf61f6cafdbe11d99b1ee9eb27c95a3c46ba9d7b
NightmareYandere <36663403+Dorthion@users.noreply.github.com>
2019-12-19 14:46:23 +0100

    LocalServer
    Dodano LocalServer

---
+++ LocalServer/API/Domain/Models/DispenserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.API.Domain.Models
+{
+    public class Dispenser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool TurnDiode { get; set; }
+    }
+}
---
+++ LocalServer/API/Domain/Services/DispenserInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.API.Domain.Models;
+
+namespace WebApplication1.Domain.Services
+{
+    public interface IDispenserInterface
+    {
+        Task<IEnumerable<Dispenser>> ListAsync();
+    }
+}
