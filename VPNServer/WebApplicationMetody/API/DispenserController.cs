5356734205eb7653f3a00f397755c477c0afc7a0
NightmareYandere <36663403+Dorthion@users.noreply.github.com>
2020-01-09 21:39:06 +0100

    Dodaje VPNServer
    DispenserMetody - Wersja Publiczna
    WebApplicationMetody - Projekt

---
+++ VPNServer/WebApplicationMetody/API/DispenserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationMetody.API
+{
+    public class DispenserController : Controller
+    {
+        [Route("/api/Post/")]
+        public ActionResult Post()
+        {
+            string s = System.IO.File.ReadAllText(@"Test.txt");
+            int temp = Convert.ToInt32(s);
+            if (temp == 0)
+                temp = 1;
+            else temp = 0;
+            System.IO.File.WriteAllText(@"Test.txt", temp.ToString());
+            return Content(temp.ToString());
+        }
+
+        [Route("/api/Get/")]
+        public ActionResult Get()
+        {
+            string s = System.IO.File.ReadAllText(@"Test.txt");
+            int temp = Convert.ToInt32(s);
+            return Content(temp.ToString());
+        }
+
+        [Route("/api/Display/")]
+        public bool TestGet()
+        {
+            return true;
+        }
+
+        [Route("/api/GetInt/")]
+        public int Get2()
+        {
+            string s = System.IO.File.ReadAllText(@"Test.txt");
+            int temp = Convert.ToInt32(s);
+            return temp;
+        }
+
+        [Route("/api/PostInt/")]
+        public int Post2()
+        {
+            string s = System.IO.File.ReadAllText(@"Test.txt");
+            int temp = Convert.ToInt32(s);
+            if (temp == 0)
+                temp = 1;
+            else temp = 0;
+            System.IO.File.WriteAllText(@"Test.txt", temp.ToString());
+            return temp;
+        }
+    }
+}
---
+++ VPNServer/WebApplicationMetody/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApplicationMetody
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
