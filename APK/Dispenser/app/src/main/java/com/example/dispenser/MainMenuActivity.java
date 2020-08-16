b3e58e790c2683c687a4d1f5690515639e4df4bc
wgpol098 <38254999+wgpol098@users.noreply.github.com>
2020-08-16 16:32:59 +0200

    Deleted usless data

--- APK/Dispenser/app/src/main/java/com/example/dispenser/MainMenuActivity.java
+++ APK/Dispenser/app/src/main/java/com/example/dispenser/MainMenuActivity.java
@@ -1,14 +1,19 @@
 package com.example.dispenser;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.renderscript.ScriptGroup;
 import android.util.JsonReader;
 import android.view.View;
@@ -24,121 +29,49 @@ import java.net.URLConnection;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity
+{
 
+    int IdDispenser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main_menu);
+
+        //Oczytywanie przekazanych danych
+        Bundle b = getIntent().getExtras();
+        if(b!=null) IdDispenser = b.getInt("IdDispenser");
+        else finish();
+
+        //Ukrywanie buttona settings
+        Button btn = findViewById(R.id.SettingButton);
+        btn.setVisibility(View.INVISIBLE);
+    }
+
+    public void fCalendarButton(View v)
+    {
+        Intent intent = new Intent(this,CalendarActivity.class);
+        intent.putExtra("idDispenser",IdDispenser);
+        startActivity(intent);
+    }
+    public void fSettingsButton(View v)
+    {
+        Intent intent = new Intent(this,SettingActivity.class);
+        startActivity(intent);
+    }
     public void fHistoryButton(View v)
     {
         Intent intent = new Intent(this,HistoryActivity.class);
+        intent.putExtra("IdDispenser",IdDispenser);
         startActivity(intent);
     }
 
     public void fHoursButton(View v)
     {
         Intent intent = new Intent(this,HoursActivity.class);
+        intent.putExtra("IdDispenser",IdDispenser);
         startActivity(intent);
-    }
-    public void fGetButton(View v)
-    {
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                HttpURLConnection ASPNETConnection = null;
-                try
-                {
-                    URL ASPNETURL = new URL("http://192.168.137.1/Api/Dispenser/Post");
-                    //URL ASPNETURL = new URL("aktualnyAdresIP/api/Dispenser/Post");
-                    ASPNETConnection = (HttpURLConnection) ASPNETURL.openConnection();
-
-                    //InputStream responseBody = ASPNETConnection.getInputStream();
-                    //InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
-                    //JsonReader jsonReader = new JsonReader(responseBodyReader);
-
-                    if (ASPNETConnection.getResponseCode() == 200)
-                    {
-                        DialogFragment dialog = new MyDialog("Sukces","Działa");
-                        dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-
-                    }
-                    else
-                    {
-                        //Connection not successfull
-                        DialogFragment dialog = new MyDialog("Błąd","Połączenie nie powiodło się");
-                        dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-                    }
-
-                }
-                catch (MalformedURLException e)
-                {
-                    //bad  URL, tell the user
-                    DialogFragment dialog = new MyDialog("Błąd","Zły adres URL");
-                    dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-                }
-                catch (IOException e)
-                {
-                    //network error/ tell the user
-                    DialogFragment dialog = new MyDialog("Błąd","Aplikacja nie ma dostępu do internetu!");
-                    dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-                }
-                finally
-                {
-                    if(ASPNETConnection != null) ASPNETConnection.disconnect();
-                }
-/*
-                    ASPNETConnection = (HttpsURLConnection) ASPNETURL.openConnection();
-                    ASPNETConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-            /*
-            if (dataBaseConnection.getResponseCode() == 200)
-            {
-                dataBaseConnection.setRequestMethod("POST");
-                // Create the data
-                String myData = "message=Hello";
-
-                // Enable writing
-                dataBaseConnection.setDoOutput(true);
-
-                // Write the data
-                dataBaseConnection.getOutputStream().write(myData.getBytes());
-                // Success
-                // Further processing here
-            }
-            else {
-                // Error handling code goes here
-            }
-
-                }
-
-                catch (MalformedURLException e)
-                {
-                    //bad  URL, tell the user
-                    DialogFragment dialog = new MyDialog("Eloszka","Message");
-                    dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-                }
-
-                catch (IOException e)
-                {
-                    //network error/ tell the user
-                    DialogFragment dialog = new MyDialog("Eluwina","Message");
-                    dialog.show(getSupportFragmentManager(), "MyDialogFragmentTag");
-                }
-
-                finally
-                {
-                    ASPNETConnection.disconnect();
-                }
-                */
-
-            }
-        });
-    }
-    Button hisButton;
-    Button hourButton;
-    Button getButton;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-        hisButton = findViewById(R.id.HistoryButton);
-        hourButton = findViewById(R.id.HoursButton);
-        getButton =  findViewById(R.id.GetButton);
     }
 }
--- APK/Dispenser/app/src/main/java/com/example/dispenser/MyDialog.java
+++ APK/Dispenser/app/src/main/java/com/example/dispenser/MyDialog.java
@@ -2,6 +2,7 @@ package com.example.dispenser;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.fragment.app.DialogFragment;
 
@@ -10,25 +11,53 @@ public class MyDialog extends DialogFragment
 {
     String Title;
     String Message;
+    String Type="OK";
     public MyDialog(String Title_, String Message_)
     {
-        Title=Title_;
-        Message=Message_;
+        Title = Title_;
+        Message = Message_;
     }
+
+    public MyDialog(String Title_, String Message_, String Type_)
+    {
+        Title = Title_;
+        Message = Message_;
+        Type = Type_;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(Title);
         builder.setMessage(Message);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        if(Type.equals("OK"))
         {
-            public void onClick(DialogInterface dialog,int id)
+            builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener()
             {
+                public void onClick(DialogInterface dialog,int id)
+                {
 
-            }
-        });
+                }
+            });
+        }
+        if(Type.equals("YESNO"))
+        {
+            builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
 
+                }
+            });
+            builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    dialog.cancel();
+                }
+            });
+        }
         return builder.create();
     }
 }
